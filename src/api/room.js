f239286b8ad8a60127972a35f39abe4a1405039b
hj-language <idgafff@kumoh.ac.kr>
2022-02-14 00:55:55 +0900

    2/14 room create, delete 구현,  test 쿼리 작성

--- src/api/room.js
+++ src/api/room.js
@@ -1,51 +1,91 @@
 const express = require('express');
+const mysql = require('mysql');
 const router = express.Router();
 
 const dbConn = require('../conn.js').init();
 
-function checkWithDB(id) {
-    let query = 'SELECT * FROM MEMBER WHERE id=?;';
-    return new Promise((resolve, reject) => {
-        dbConn.query(query, [id], (err, rows) => {
-            if (!err){  // DB 커넥션 성공
-                console.log(query, id, "connect success");
-                resolve(rows[0]);
-            }
-            console.log(err);
-            reject();
-        })
-    });
-}
+/*
+    - test path: http://localhost:3000/api/rooms/test
+    - 토큰 검증 미들웨어 추가하기
+*/
 
-function insertIntoDB(query, items) {
+router.get('/test', async (req, res, next) => {
+    function test(sql, obj) {
+        return new Promise((res, rej) => {
+            dbConn.query(sql, obj, (err, rows) => {
+                if (!err)
+                {
+                    console.log(rows);
+                    res();
+                }
+                else {
+                    console.log(err);
+                    rej();
+                }
+            })
+        })
+    }
+    //await test("DELETE FROM room where roomname='myROOM'", []);
+    await test("SELECT * FROM room", []);
+    res.send("ok");
+});
+
+function SendQuery(sql, obj) {
     return new Promise((resolve, reject) => {
-        dbConn.query(query, items, (err, rows) => {
+        dbConn.query(sql, obj, (err, rows) => {
             if (!err) {
-                console.log(query, items, "connect success");
-                resolve();
+                console.log(sql, obj, "connect success");
+                resolve(rows);
             }
             console.log(err);
-            reject();
+            reject(null);
         })
     })
 }
 
-router.post('/rooms', (req, res, next) => {
-    let room = req.body;
-    let query = "SELECT * FROM ROOMS;"
-    dbConn.query(query, (err, rows) => {
-        if (!err)
-        {
-            console.log(rows[0]);
-        }
-    })
+router.get('/', (req, res, next) => {
+    /*
+        LIST: /api/rooms
+        SEARCH: /api/rooms?start-point="a"&end-point="b"
+        query 이용하여 start point, end point 받기
+    */
 });
 
-// GET /room/update, GET /room/view
-router.get('/rooms/:id', (req, res, next) => {
+router.post('/', async (req, res, next) => {
+    let roomObj = {
+        roomname: req.body.roomname,
+        startpoint: req.body.startpoint,
+        endpoint: req.body.endpoint,
+        starttime: req.body.starttime,
+        currentmember: req.body.currentmember,
+        totalmember: req.body.totalmember,
+        createtime: new Date()
+    }
+
+    if (await SendQuery("INSERT INTO room SET ?", roomObj))
+        res.status(200);
+    else
+        res.status(400);
+
+    res.send();
 });
 
-router.put('/rooms/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
+    let row = await SendQuery("SELECT * FROM room WHERE roomid=?", req.params.id);
+    if (row) {
+        /* 해당 id의 room이 존재하면 아래 넘겨주기
+        leaderid, roomname, startpoint, endpoint, starttime, currentmember, totalmember, createtime, comments, isRide
+        comments랑 isRide는 테이블 따로 만들고 거기에서 받아와야 함
+        내 생각엔 클라이언트의 유저 id를 req.body로 안 받고 query로 받아도 될 것 같음
+        */
+        res.status(200);
+    }
+    else {
+        res.status(400);
+    }
+});
+
+router.put('/:id', (req, res, next) => {
     /*
         UPDATE: /api/rooms/1
         RIDE IN/OUT: /api/rooms/1?isRide=true or false
@@ -53,15 +93,12 @@ router.put('/rooms/:id', (req, res, next) => {
     */
 });
 
-router.delete('/rooms/:id', (req, res, next) => {
-});
-
-router.get('/rooms', (req, res, next) => {
-    /*
-        LIST: /api/rooms
-        SEARCH: /api/rooms?start-point="a"&end-point="b"
-        query 이용하여 start point, end point 받기
-    */
+router.delete('/:id', (req, res, next) => {
+    if (await SendQuery("DELETE FROM room where roomid=?", req.params.id))
+        res.status(200);
+    else
+        res.status(400);
+    res.send();
 });
 
 module.exports = router;
