022d8afb13d46b9abfa99ecbc10edbf6e99e9dd5
hj-language <idgafff@kumoh.ac.kr>
2022-02-18 22:04:52 +0900

    2/18 cors 미들웨어 등록

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const bodyParser = require('body-parser')
 
 let port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', function(req, res) {
     res.send('Test Server');
